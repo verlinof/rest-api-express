35c33d3633742f24b51a9dea9d7da222afe88d75
verlinof <ajikoko.zx@gmail.com>
2024-03-19 14:20:37 +0700

    Authentication

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const postRoute = require('./routes/post');
+const userRoute = require('./routes/user');
 
 app.use(bodyParser.json());
 //Routes
 app.use('/posts', postRoute);
+app.use('/auth', userRoute);
 
 module.exports = app
---
+++ routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const userController = require('../controllers/user.controller');
+
+const router = express.Router();
+
+//Route
+router.post('/login', userController.login);
+router.post('/register', userController.register);
+
+module.exports = router
