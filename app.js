88d2d6617c459c2f85ffa068e0c755614da7458a
verlinof <ajikoko.zx@gmail.com>
2024-03-19 22:10:23 +0700

    File Uploading using Multer

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const app = express();
 
 const postRoute = require('./routes/post');
 const userRoute = require('./routes/user');
+const imageRoute = require('./routes/image');
 
 app.use(bodyParser.json());
+
+//Static for images
+app.use('/uploads', express.static('uploads'));
 //Routes
 app.use('/posts', postRoute);
 app.use('/auth', userRoute);
+app.use('/images', imageRoute);
 
 module.exports = app
