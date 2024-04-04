d24ca150171f762cec842ceefad5210c4d0c8387
verlinof <ajikoko.zx@gmail.com>
2024-04-04 22:22:56 +0700

    Feat: Swagger Documentation

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const specs = require('./swagger');
 
 const app = express();
 
@@ -19,5 +21,7 @@ app.use('/auth', userRoute);
 app.use('/comments', commentRoute);
 app.use('/categories', categoryRoute);
 app.use('/images', imageRoute);
+//Documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 module.exports = app
